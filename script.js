f1825996b68012986da6356e66ba0e8b82e25a37
Jacob Krueger <jmk4u14@gmail.com>
2020-10-28 21:16:04 -0400

    Added assets folder, modified css for visuals, worked on api.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-function searchWeather(searchValue) {
+$("#search-button").on("click", function() {
+    var searchResult = $("#city-search").val();
+});
+
+
+function weather(searchResult) {
     $.ajax({
       type: "GET",
-      url: "http://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=5e8ef1f436a1754fa78c99c12aed203d=imperial",
-      dataType: "json",
-      success: function(data) {
-          
-      }
+      url: "http://api.openweathermap.org/data/2.5/weather?q=" + searchResult + "&appid=5e8ef1f436a1754fa78c99c12aed203d=imperial"
+    }
