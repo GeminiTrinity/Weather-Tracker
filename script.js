68df43ba007c909737825b875ea3cabff4a9ef69
Jacob Krueger <jmk4u14@gmail.com>
2020-11-01 16:33:44 -0500

    Updated divs

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-$("#search-button").on("click", function(event) {
-    event.preventDefault();
-    var city = $(".city-search").val();
+$("#city-search").on("click", function(event) {
+  event.preventDefault();
+  var city = $("#city-input").val();
+  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=5e8ef1f436a1754fa78c99c12aed203d";
 
-    $.ajax({
-      url: "http://api.openweathermap.org/data/2.5/weather?q=" + city +"&appid=5e8ef1f436a1754fa78c99c12aed203d",
-      method: "GET"
-    }).then(function(response) {
-        console.log(response)
-    })
-
-});
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+      console.log(response);
+  });
+})
